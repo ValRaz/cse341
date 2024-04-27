1531c35188a89ef525345bfa2f445e908d331698
ValRaz <valerie.dres@gmail.com>
2024-04-26 21:47:23 -0600

    troubleshooting

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/index.js');
 
 const port = 3001;
-
-app.use('/', require('.routes/index.js'));
+app.use('/', require(routes));
 
 app.listen(process.env.PORT || port, () => {
     console.log('Web Server is listening at port ' + (process.env.PORT || port));
