f36ce9b5a70a84763ce5aaa19d5017eb7c39390a
ValRaz <valerie.dres@gmail.com>
2024-04-26 22:28:29 -0600

    code cleanup finale push

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/index.js');
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.use('/', routes);
 
-app.listen(process.env.PORT || port, () => {
-    console.log('Web Server is listening at port ' + (process.env.PORT || port));
+app.listen(port, () => {
+    console.log(`Server is listening at port ${port}`);
   });
