f003531dd211bf5fb1fc546ef5f11a8623dd9371
ValRaz <valerie.dres@gmail.com>
2024-04-26 19:45:47 -0600

    controllers update

---
+++ controllers/lesson1.js
@@ -0,0 +1,17 @@
+const ericRoute = app.get('/', (req, res) => {
+    res.send("Eric Nord");
+});
+
+const alexRoute = app.get('/alex', (req, res) => {
+    res.send("Alex Raz");
+});
+
+const valRoute = app.get('/val', (req, res) => {
+    res.send("Val Raz");
+});
+
+module.exports = {
+  ericRoute,
+  alexRoute,
+  valRoute
+}
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000
- 
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
+const port = 3000;
+const lesson1Controller = require('./controllers/lesson1.js')
+
+app.get('/', lesson1Controller.ericRoute);
+
+app.get('/alex', lesson1Controller.alexRoute);
+
+app.get('/val', lesson1Controller.valRoute);
 
 app.listen(process.env.PORT || port, () => {
     console.log('Web Server is listening at port ' + (process.env.PORT || port));
