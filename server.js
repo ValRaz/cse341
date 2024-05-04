d308c1a80a9d88127fca23aca6bf9c3cb7b25b3d
ValRaz <valerie.dres@gmail.com>
2024-05-04 17:57:24 -0600

    code cleanup

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/index.js');
 const mongodb = require('./db/dbconnect');
 
 const port = process.env.PORT || 3001;
