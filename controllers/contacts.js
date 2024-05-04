4f58fec97fc6ea4421d0b5a1e496d5bddd1caa5c
ValRaz <valerie.dres@gmail.com>
2024-05-04 17:32:28 -0600

    code cleanup

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,17 +2,17 @@ const mongodb = require('../db/dbconnect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getOne = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb()
-      .db()
-      .collection('Contacts')
-      .find({ _id: userId });
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
-    });
-  };
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection('Contacts')
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
   
   const getAll = async (req, res, next) => {
     const result = await mongodb.getDb().db().collection('Contacts').find();
