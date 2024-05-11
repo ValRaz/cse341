35475ee5b4c4ff411a45e91f31a57ba7af1f5cc2
ValRaz <valerie.dres@gmail.com>
2024-05-11 11:17:58 -0600

    body parser added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const mongodb = require('./db/dbconnect');
 
 const port = process.env.PORT || 3001;
 
+app.use(bodyParser.json());
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  next();
+})
 app.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
