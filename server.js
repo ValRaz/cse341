5664909942db4bd86633270a3911b2ea02421519
ValRaz <valerie.dres@gmail.com>
2024-05-18 08:03:11 -0600

    code cleanup cors installed

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const mongodb = require('./db/dbconnect');
-const cors = require('cors');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+
+const app = express();
 
 const port = process.env.PORT || 3001;
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
-
 app.use(bodyParser.json());
-app.use((req,res,next) => {
-  res.setHeader('Access-Control-Allow-Origin','*');
-  next();
-})
 app.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
